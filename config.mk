f0d4f9c4ea2b5a5e2d4fc2ad0ecd79263f83b77c
CPU_Code <923992029@qq.com>
2020-05-14 23:17:05 +0800

    顶层makefile定义变量

--- config.mk
+++ config.mk
@@ -1,3 +1,11 @@
+ #
+ # @Author: cpu_code
+ # @Date: 2020-05-14 10:12:50
+ # @LastEditTime: 2020-05-14 23:12:40
+ # @FilePath: \u_boot_fslc\config.mk
+ # @Gitee: https://gitee.com/cpu_code
+ # @CSDN: https://blog.csdn.net/qq_44226094
+ #
 # SPDX-License-Identifier: GPL-2.0+
 #
 # (C) Copyright 2000-2013
@@ -10,6 +18,8 @@
 # (Tegra needs different flags for SPL.
 #  That's the reason why this file must be included from spl/Makefile too.
 #  If we did not have Tegra SoCs, build system would be much simpler...)
+
+#  5 个变量在 uboot 根目录下的.config 文件中有定义
 PLATFORM_RELFLAGS :=
 PLATFORM_CPPFLAGS :=
 PLATFORM_LDFLAGS :=
@@ -21,7 +31,9 @@ OBJCOPYFLAGS :=
 VENDOR :=
 #########################################################################
 
+# 定义变量 ARCH ，值为 $(CONFIG_SYS_ARCH:"%"=%) ，也就是提取 CONFIG_SYS_ARCH 里面双引号“ ”之间的内容
 ARCH := $(CONFIG_SYS_ARCH:"%"=%)
+# 定义变量 CPU，值为$(CONFIG_SYS_CPU:"%"=%)
 CPU := $(CONFIG_SYS_CPU:"%"=%)
 ifdef CONFIG_SPL_BUILD
 ifdef CONFIG_TEGRA
@@ -42,13 +54,19 @@ endif
 # CPU-specific code.
 CPUDIR=arch/$(ARCH)/cpu$(if $(CPU),/$(CPU),)
 
+#  sinclude 和 include 的功能类似，在 Makefile 中都是读取指定文件内容
+# 读取文件$(srctree)/arch/$(ARCH)/config.mk 
+#  sinclude 读取的文件如果不存在的话不会报错
 sinclude $(srctree)/arch/$(ARCH)/config.mk	# include architecture dependend rules
+
+# 读取文件$(srctree)/$(CPUDIR)/config.mk 的内容
 sinclude $(srctree)/$(CPUDIR)/config.mk		# include  CPU	specific rules
 
 ifdef	SOC
 sinclude $(srctree)/$(CPUDIR)/$(SOC)/config.mk	# include  SoC	specific rules
 endif
 ifneq ($(BOARD),)
+# 定义变量 BOARDDIR ，如 定义了 VENDOR 那么BOARDDIR=$(VENDOR)/$(BOARD)，否则的 BOARDDIR=$(BOARD)
 ifdef	VENDOR
 BOARDDIR = $(VENDOR)/$(BOARD)
 else
@@ -56,6 +74,7 @@ BOARDDIR = $(BOARD)
 endif
 endif
 ifdef	BOARD
+# 读取文件$(srctree)/board/$(BOARDDIR)/config.mk
 sinclude $(srctree)/board/$(BOARDDIR)/config.mk	# include board specific rules
 endif
 
